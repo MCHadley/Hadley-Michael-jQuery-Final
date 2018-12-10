6f045a7b3557d2df5b4faca6cdcd96d4179684ea
Michael <michael.c.hadley@gmail.com>
2018-12-10 16:27:30 -0500

    Added active link class and ext links open in new window

--- js/functions.js
+++ js/functions.js
@@ -11,3 +11,19 @@ function faqAccord(){
     animate: 400
   });
 }
+
+function navLinks(){
+  var url = window.location.href; //grab full URL
+  var link = url.substr(url.lastIndexOf('/') + 1); //get end of URL
+  var projLink = url.substr(url.lastIndexOf('project') + 10) //grab current page
+  var duckName = projLink.startsWith('Duck'); //grab and verify project Name
+  
+  //compare project name and apply correct current link class
+  if(duckName == true){
+    $('[href*="' + link + '"]').addClass("pageHighlight");  
+  }else{
+    $('[href*="' + link + '"]').addClass("currentLink");
+    }
+  
+    $("a[href^='http://']").attr('target', '_blank'); // make external links open in new window/tab
+}
